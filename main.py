12d8fac5f844fa9dc592681d1aeb2545d90e8ca9
elbekdavron <elbekd4vronov@gmail.com>
2025-01-28 17:52:03 +0500

    added first student

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ db = TinyDB('students.json')
 
 # Reference the default table
 students_table = db.table('students')
+
+student = {    
+        "id": 192,
+        "name": "Shamsiddin Nishonov",
+        "age": 15,
+        "gender": "Male",
+        "contact": "911234567",
+        "grade_level": "Grade 10",
+        "subjects": {
+            "math": 57,
+            "science": 77,
+            "english": 93
+        },
+        "attendance": 87,
+        "activities": ["Online games", "Debate Club"],
+        "address": {
+            "street": "Buyuk ipak yo'li",
+            "city": "Samarkand",
+            "state": "Sam",
+            "zip_code": "10400"
+        }
+    } 
+
+db.insert(student)
